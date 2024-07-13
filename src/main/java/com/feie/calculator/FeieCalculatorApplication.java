48cd893b5825582ffb9553fe8d30f20c81165c01
Ian <harperi@tcd.ie>
2024-07-13 18:55:52 +0200

    Initial commit with a working webpage

---
+++ src/main/java/com/feie/calculator/FeieCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.feie.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeieCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeieCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feie/calculator/FeieCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feie.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeieCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
