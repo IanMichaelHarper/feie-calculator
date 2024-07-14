98b67357cd0bf7a81c08f2ed14cc41c8b8482d5a
Ian <harperi@tcd.ie>
2024-07-14 11:55:54 +0200

    Try do calculation, need to fix

--- src/main/java/com/feie/calculator/FeieCalculatorService.java
+++ src/main/java/com/feie/calculator/FeieCalculatorService.java
@@ -1,17 +1,77 @@
 package com.feie.calculator;
 
 import com.feie.calculator.models.TaxResults;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
 
 @Service
 public class FeieCalculatorService {
 
-    public TaxResults calculateIncomeAfterTax(String income) {
-        BigDecimal incomeAfterFEIE = BigDecimal.ZERO;
-        BigDecimal federalTax = BigDecimal.ZERO;
-        BigDecimal incomeAfterTax= BigDecimal.ZERO;
+    private static final Logger log = LoggerFactory.getLogger(FeieCalculatorService.class);
+
+    public TaxResults calculateIncomeAfterTax(String inc) {
+        // most code here is from Venice.ai
+        if (inc == null) {
+            return null;
+        }
+        double income = Double.parseDouble(inc);
+
+        if (income <= 0) {
+            System.out.println("Please enter a valid income.");
+            return null;
+        }
+
+        double FEIEAmount = 108700; // TODO: Maximum exclusion amount for 2022
+        double[][] federalTaxBrackets = {
+//                {0.10, 0},
+                {0.10, 40000},
+                {0.12, 80000},
+                {0.22, 170000},
+                {0.24, 215000},
+                {0.32, 500000},
+                {0.35, 1000000},
+                {0.37, 1000000}
+                // TODO: remainder
+        };
+
+        double[] federalTaxRates = new double[federalTaxBrackets.length];
+        double[] federalTaxThresholds = new double[federalTaxBrackets.length];
+
+        for (int i = 0; i < federalTaxBrackets.length; i++) {
+            federalTaxRates[i] = federalTaxBrackets[i][0];
+            federalTaxThresholds[i] = federalTaxBrackets[i][1];
+        }
+
+//        double taxableIncome = Math.max(income - FEIEAmount, 0);
+        double taxableIncome = income;
+        double federalTax = 0;
+        double feieRebate = 0;
+        for (int i = 0; i < federalTaxBrackets.length; i++) {
+            double[] bracket = federalTaxBrackets[i];
+            if (taxableIncome <= bracket[1]) {
+
+//                log.info(String.format("taxableIncome %,.2f * bracket[0] %,.2f "), taxableIncome, bracket[0]);
+                federalTax += taxableIncome * bracket[0];
+                break;
+            }
+//            log.info(String.format("bracket[1] %,.2f * bracket[0] %,.2f"), bracket[1], bracket[0]);
+            federalTax += bracket[1] * bracket[0];
+            taxableIncome -= bracket[1];
+        }
+
+        if (income < FEIEAmount) {
+            feieRebate = federalTax;
+        }
+        else {
+            // TODO implement
+        }
+        double incomeAfterFEIE = Math.min(income, FEIEAmount);
+        double incomeAfterTax = incomeAfterFEIE - federalTax;
+
+        System.out.printf("Income after FEIE and federal tax: $%.2f%n", incomeAfterTax);
+        System.out.printf("Federal tax: $%.2f%n", federalTax);
+        System.out.printf("Tax without FEIE: $%.2f%n", income * 0.24);
 
         TaxResults taxResults = new TaxResults();
         taxResults.setIncomeAfterFEIE(incomeAfterFEIE);
--- src/main/java/com/feie/calculator/models/TaxResults.java
+++ src/main/java/com/feie/calculator/models/TaxResults.java
@@ -1,34 +1,32 @@
 package com.feie.calculator.models;
 
-import java.math.BigDecimal;
-
 public class TaxResults {
-    private BigDecimal incomeAfterFEIE;
-    private BigDecimal federalTax;
-    private BigDecimal incomeAfterTax;
+    private double incomeAfterFEIE;
+    private double federalTax;
+    private double incomeAfterTax;
 
     // Getters and setters
-    public BigDecimal getIncomeAfterFEIE() {
+    public double getIncomeAfterFEIE() {
         return incomeAfterFEIE;
     }
 
-    public void setIncomeAfterFEIE(BigDecimal incomeAfterFEIE) {
+    public void setIncomeAfterFEIE(double incomeAfterFEIE) {
         this.incomeAfterFEIE = incomeAfterFEIE;
     }
 
-    public BigDecimal getFederalTax() {
+    public double getFederalTax() {
         return federalTax;
     }
 
-    public void setFederalTax(BigDecimal federalTax) {
+    public void setFederalTax(double federalTax) {
         this.federalTax = federalTax;
     }
 
-    public BigDecimal getIncomeAfterTax() {
+    public double getIncomeAfterTax() {
         return incomeAfterTax;
     }
 
-    public void setIncomeAfterTax(BigDecimal incomeAfterTax) {
+    public void setIncomeAfterTax(double incomeAfterTax) {
         this.incomeAfterTax = incomeAfterTax;
     }
 }
